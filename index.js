9f87cc963605259ea3f788e5b5d17f9ca87e8cec
Bakhos Rachwan <bakhos.rachwan@gmail.com>
2020-11-12 15:18:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
+<<<<<<< HEAD
 const append = ["Milo", "Otis", "Garfield", "Odie"];
 const prepend = ["Odie", "Milo", "Otis", "Garfield"];
 const removeLast = ["Milo", "Otis"];
 const removeFirst = ["Otis", "Garfield"];
 
+=======
+// Write your solution here!
+pets = ["Milo", "Otis", "Garfield"]
+const append.push =["Odie"];
+>>>>>>> 8bd80801fdb5157dca04da8415b38116a791f8ec
